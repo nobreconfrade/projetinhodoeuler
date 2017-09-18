0e5a3eaade2da472e7ca3dfb5e28d98446e9baad
nobreconfrade <willpereirabr@gmail.com>
2017-09-18 18:48:43 -0300

    p1

---
+++ p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int v[1000] = {0};
+  int count = 0;
+  for (int i = 0; i < 1000; i++) {
+    if((i%3 == 0) || (i%5 == 0)){
+      v[i]++;
+    }
+  }
+  for (int i = 0; i < 1000; i++) {
+    if (v[i] > 0) {
+      count += i;
+    }
+  }
+  cout << count << '\n';
+  return 0;
+}
