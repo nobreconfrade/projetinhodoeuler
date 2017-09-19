8a743e70014b6014f1057a9c0dc0598528d71413
nobreconfrade <willpereirabr@gmail.com>
2017-09-19 09:34:08 -0300

    solving p2

---
+++ p2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+#define SIZE 4000000
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int x1,x2,option;
+  x1 = 1;
+  x2 = 2;
+  option = 0;
+  while (x1<SIZE) || (x2<SIZE) {
+    if(option == 0){
+      if (x2 ==) {
+        /* code */
+      }
+      x1 = x1 + x2;
+    }
+    if (option == 1) {
+      x2 = x1 + x2;
+    }
+  }
+  return 0;
+}
